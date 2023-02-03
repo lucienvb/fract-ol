6228a296c22040d338877faab0551d0b5998fc4d
lvan-bus <lucienvanbussel@gmail.com>
2023-02-03 11:47:52 +0100

    rewrote give_color_rainbow: now with loop construction instead of hard code

--- src/color.c
+++ src/color.c
@@ -34,49 +34,48 @@ static void	give_color_blue_green(mlx_image_t *g_img, int x, int y, int iteratio
 		mlx_put_pixel(g_img, x, y, 0x61b3ffFF);
 }
 
-static void	give_color_rainbow(mlx_image_t *g_img, int x, int y, int iterations)
-{
-	if (iterations == MAX_ITERATIONS)
-		mlx_put_pixel(g_img, x, y, 0x9400D3FF);
-	else if (iterations > (0.85 * MAX_ITERATIONS) && iterations < MAX_ITERATIONS)
-		mlx_put_pixel(g_img, x, y, 0x4B0082FF);
-	else if (iterations > (0.7 * MAX_ITERATIONS) && iterations < (0.85 * MAX_ITERATIONS))
-		mlx_put_pixel(g_img, x, y, 0x0000FFFF);
-	else if (iterations > (0.55 * MAX_ITERATIONS) && iterations < (0.7 * MAX_ITERATIONS))
-		mlx_put_pixel(g_img, x, y, 0x00FF00FF);
-	else if (iterations > (0.4 * MAX_ITERATIONS) && iterations < (0.55 * MAX_ITERATIONS))
-		mlx_put_pixel(g_img, x, y, 0xFFFF00FF);
-	else if (iterations > (0.2 * MAX_ITERATIONS) && iterations < (0.4 * MAX_ITERATIONS))
-		mlx_put_pixel(g_img, x, y, 0xFF7F00FF);
-	else
-		mlx_put_pixel(g_img, x, y, 0xFF0000FF);
-}
+//static void	give_color_rainbow(mlx_image_t *g_img, int x, int y, int iterations)
+//{
+//	if (iterations == MAX_ITERATIONS)
+//		mlx_put_pixel(g_img, x, y, 0x9400D3FF);
+//	else if (iterations > (0.85 * MAX_ITERATIONS) && iterations < MAX_ITERATIONS)
+//		mlx_put_pixel(g_img, x, y, 0x4B0082FF);
+//	else if (iterations > (0.7 * MAX_ITERATIONS) && iterations < (0.85 * MAX_ITERATIONS))
+//		mlx_put_pixel(g_img, x, y, 0x0000FFFF);
+//	else if (iterations > (0.55 * MAX_ITERATIONS) && iterations < (0.7 * MAX_ITERATIONS))
+//		mlx_put_pixel(g_img, x, y, 0x00FF00FF);
+//	else if (iterations > (0.4 * MAX_ITERATIONS) && iterations < (0.55 * MAX_ITERATIONS))
+//		mlx_put_pixel(g_img, x, y, 0xFFFF00FF);
+//	else if (iterations > (0.2 * MAX_ITERATIONS) && iterations < (0.4 * MAX_ITERATIONS))
+//		mlx_put_pixel(g_img, x, y, 0xFF7F00FF);
+//	else
+//		mlx_put_pixel(g_img, x, y, 0xFF0000FF);
+//}
 
+
+#include <stdio.h>
 // try to fix the coloring with while loops instead of hard coding it
-/*static void	give_color_rainbow(mlx_image_t *g_img, int x, int y, int iterations)
+static void	give_color_rainbow(mlx_image_t *g_img, uint32_t x, uint32_t y, int iterations)
 {
 	int 			i;
 	int				j;
 	size_t 			k;
-	unsigned int	lst[] = {0x9400D3FF, 0x9400D3FF, 0x4B0082FF, 0x0000FFFF, 0x00FF00FF, 0xFFFF00FF, 0xFF7F00FF, 0xFF0000FF};
+	uint32_t 		lst[] = {0x9400D3FF, 0x9400D3FF, 0x4B0082FF, 0x0000FFFF, 0x00FF00FF, 0xFFFF00FF, 0xFF7F00FF, 0xFF0000FF};
 
 	i = MAX_ITERATIONS;
-	j = 1;
-	k = 0;
-	while(j > 0 && i > 0)
+	k = 1;
+	if (iterations == i)
+		mlx_put_pixel(g_img, x, y, lst[0]);
+	j = i - 15;
+	while (i > 0)
 	{
-		if (iterations == i && j == 0)
-		{
+		if (iterations < i && iterations > j)
 			mlx_put_pixel(g_img, x, y, lst[k++]);
-		}
-		else if (iterations < i && iterations > j)
-		{
-			mlx_put_pixel(g_img, x, y, lst[k++]);
-			i -= 15;
-		}
-		j = i -15;
+		k++;
+		i = j;
+		j -= 15;
 	}
-}*/
+}
 
 static void	give_color_black_and_white(mlx_image_t *g_img, int x, int y, int iterations)
 {
